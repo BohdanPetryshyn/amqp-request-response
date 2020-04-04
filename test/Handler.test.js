091ebc4e3087d1db2ba1963dce9646cd5d9a32bb
BohdanPetryshyn <b.y.petryshyn@gmail.com>
2020-04-04 11:31:30 +0300

    Add testing describe section and test request handler call with deserialized payload

--- test/Handler.test.js
+++ test/Handler.test.js
@@ -1,22 +1,36 @@
 const Handler = require('../main/Handler');
 
-const QUEUE_NAME = 'queue name';
-
-let channel;
-let handler;
-
-beforeEach(() => {
-  channel = {
-    assertQueue: jest.fn(),
-    consume: jest.fn(),
-    sendToQueue: jest.fn(),
-    ack: jest.fn(),
+describe('Handler', () => {
+  const QUEUE_NAME = 'queue name';
+  const REQUEST_PAYLOAD = 'test request payload';
+  const REQUEST = {
+    payload: REQUEST_PAYLOAD,
+    correlationId: 'test correlationID',
+    replyTo: 'test replyTo',
   };
-  handler = new Handler(channel);
-});
+  const REQUEST_MSG = { content: Buffer.from(JSON.stringify(REQUEST)) };
 
-test('handler asserts queue before start consuming it', () => {
-  handler.handle(QUEUE_NAME, jest.fn());
+  let channel;
+  let handler;
 
-  expect(channel.assertQueue.mock.calls.length).toBe(1);
+  beforeEach(() => {
+    channel = {
+      assertQueue: jest.fn(),
+      consume: jest.fn(),
+      sendToQueue: jest.fn(),
+      ack: jest.fn(),
+    };
+    handler = new Handler(channel);
+  });
+
+  it('provides request payload to the request handler', async () => {
+    channel.consume.mockImplementation((queueName, consumer) =>
+      consumer(REQUEST_MSG)
+    );
+    const requestHandler = jest.fn();
+
+    await handler.handle(QUEUE_NAME, requestHandler);
+
+    expect(requestHandler.mock.calls[0][0]).toBe(REQUEST_PAYLOAD);
+  });
 });
